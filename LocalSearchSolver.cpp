4c876a45fa0328e9ce178d63d32c90c9cebe6d03
Sebastien Henry <34012647+WaterEveryDay@users.noreply.github.com>
2023-11-30 09:40:25 -0500

    time chrono + more randomness for LS solver

--- LocalSearchSolver.cpp
+++ LocalSearchSolver.cpp
@@ -1,4 +1,6 @@
 #include "LocalSearchSolver.h"
+#include <iostream>
+#include <chrono> 
 
 // Constructor
 LsSolver::LsSolver(string fname, string method, string cutoff, string seed) : TspSolver(fname, method, cutoff, seed) {
@@ -14,13 +16,20 @@ void LsSolver::solve() {
     setTour(bestTour.first);
 }
 
-pair<vector<int>,int> LsSolver::runLocalSearch(vector<vector<Pair> > adjList){
+pair<vector<int>,int> LsSolver::runLocalSearch(vector<vector<Pair>> adjList){
+    using namespace std;
+    using namespace chrono;
+
     // declare constants of simulated annealing algorithm
     double k = 20;
     double T = 100;
-    int nsteps = 1500000;
+    int nsteps = 2000000;
     int M = 5;  // lower temperature every M steps
     double coolingFactor = 0.9999;  // ratio by which we reduce temperature
+    int maxCountNoChange = 200000; // if algo does not accept new points for a certain amount of steps, then stop
+
+    // higher number of maxCountNoChange for higher dimension problems
+    int countNoChange = 0;
 
     // declare tour and cost of tour variables that will be updated iteratively
     vector<int> tour;
@@ -29,30 +38,50 @@ pair<vector<int>,int> LsSolver::runLocalSearch(vector<vector<Pair> > adjList){
     std::mt19937 engine(seed);  // will pass Random Number Generator (RNG) engine to functions that use randomness
 
     tour = select_random_tour(engine);  // get random tour
-    for (int t = 0; t < nsteps; t++){
+
+    auto start = high_resolution_clock::now(); // initialize chronometer
+    int t = 0;
+    double execTime;
+    while ( (t<nsteps) && (execTime<getCutoff()) && (countNoChange < maxCountNoChange)){
         costTour = computeTourLength(adjList , tour);
         vector<int> newTour = swap_random_pair(tour, engine);
+        // add extra swaps at the beginning
+        for(int otherswaps = 0; otherswaps < (5-t/10000); otherswaps++) {
+            newTour = swap_random_pair(newTour, engine);
+        }   
+
         int costNewTour = computeTourLength(adjList, newTour);
 
         // accept new tour if it has less cost, else accept it with a given probability defined by the constants
-        if (costNewTour < costTour){
+        double deltaE = costNewTour - costTour;  // always >0
+        
+        if (deltaE < 0){
             tour = newTour;
-        } else {
-            double deltaE = costNewTour - costTour;  // always >0
+            countNoChange = 0;
+        } else { // deltaE > 0
             deltaE = deltaE / costTour * 100; // normalize deltaE so it is unitless (in percent of regular tour)
             double prob = exp(-deltaE/(k*T));
             uniform_real_distribution<> uniform(0.0, 1.0);
             double r = uniform(engine);
-            if (r < prob)
+            if (r < prob) {
                 tour = newTour;
+                countNoChange = 0;
+            } else {
+                countNoChange++;
+            }
         }
-        if (tour == newTour)
-            costTour = costNewTour;  // update cost it tour was updated
+        //if (tour == newTour)
+        //    costTour = costNewTour;  // update cost it tour was updated
         // cout<<"iteration "<<t<<" cost: "<<costTour<<endl;  // print current cost
 
         // decrease Temperature parameter so we go "uphill" in solution space less often and converge faster
-        if (t % M == 0)
+        if (t % M == 0) {
             T = T * coolingFactor;
+        }
+        t++;
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(end - start);
+        execTime = duration.count() / 1e6;
     }
 
     // cout<<endl<<"-----Printing Local Search cost and tour (seed "<<seed<<")-----"<<endl;
